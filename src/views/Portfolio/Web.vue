17b9f697c0b39228f9a213375221bea4c01a4c24
KatouProg <katouprog@gmail.com>
2022-04-18 22:16:49 +0900

    Commit Files

---
+++ src/views/Portfolio/Web.vue
@@ -0,0 +1,105 @@
+<template>
+  <v-container grid-list-xl>
+    <h2 class="pl-4">
+      <span>Web</span>
+      <span class="green--text">Portfolio</span>
+    </h2>
+    <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+      <v-dialog
+        v-model="project.dialog"
+        lazy
+        max-width="1000"
+        v-for="project in projects"
+        :key="project.title"
+      >
+        <template v-slot:activator="{ on }">
+          <v-flex xs12 sm6 md4 lg4 xl4 v-on="on">
+            <v-card hover flat color="transparent">
+              <v-img
+                :src="project.poster"
+                :alt="project.title"
+                height="230"
+                lazy-src="./img/fluid-loader.gif"
+              ></v-img>
+              <v-card-title primary-title class="justify-center">{{project.title}}</v-card-title>
+            </v-card>
+          </v-flex>
+        </template>
+        <v-card v-if="project.dialog">
+          <v-img :src="project.poster"></v-img>
+          <v-card-text>
+            <h3 class="headline mb-0">
+              <span>WEB</span>
+            </h3>
+            <v-chip color="green" text-color="white">{{project.tech.tech1}}</v-chip>
+            <v-chip color="green" text-color="white">{{project.tech.tech2}}</v-chip>
+            <v-chip color="green" text-color="white">{{project.tech.tech3}}</v-chip>
+            <v-chip color="green" text-color="white">{{project.tech.tech4}}</v-chip>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn flat large dark color="green" :href="project.git" target="_blank">
+              <v-icon left>fab fa-github</v-icon>GitHub
+            </v-btn>
+            <v-btn large flat dark color="green" :href="project.demo" target="_blank">
+              <v-icon left>fas fa-desktop</v-icon>Demo
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Web Portfolio",
+    titleTemplate: "%s ← Katou's Place",
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "description",
+        content:
+          "Quentin BUXO's Web Portfolio Vue Vue.js Nuxt Nuxt.js HTML CSS Vuetify Axios GraphQL JavaScript Web Developer Front-End Frontend Designer App Responsive"
+      },
+      { charset: "utf-8" },
+      { property: "og:title", content: "Katou's Place" },
+      { property: "og:site_name", content: "Katou's Place" },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://katou.place" },
+      {
+        property: "og:image",
+        content: "./img/Dcz2PGx.jpg"
+      },
+      {
+        property: "og:description",
+        content:
+          "Quentin BUXO's Web Portfolio Vue Vue.js Nuxt Nuxt.js HTML CSS Vuetify Axios GraphQL JavaScript Web Developer Front-End Frontend Designer App Responsive"
+      }
+    ]
+  },
+  data() {
+    return {
+      dialog: false,
+      projects: [
+         {
+          dialog: false,
+          title: "Groupomania",
+          git: "https://github.com/KatouProg/QuentinBuxo_7_18122021_test",
+          demo: "https://youtu.be/oT3Uz9oo9W8",
+          tech: {
+            tech1: "VueJs",
+            tech2: "JavaScript",
+            tech3: "NodeJs",
+            tech4: "MySql"
+          },
+          poster: "https://zupimages.net/up/22/16/p7zm.png"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style  scoped>
+</style>
---
+++ src/views/Resume.vue
@@ -0,0 +1,202 @@
+<template>
+  <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+    <v-flex xs12 sm12 md5 lg6 xl6 class="mt-4 pt-4">
+      <h2 class="mb-4 pl-4 pt-2">Education</h2>
+      <v-card-text class="py-0">
+        <v-timeline align-top dense>
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">JUL 2021 -</div>
+                <div class="caption font-weight-bold green--text">MAR 2022</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Full Stack Web Development Graduate</strong>
+                <div class="caption mb-2">OPENCLASSROOMS</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">SEP 1998 -</div>
+                <div class="caption font-weight-bold green--text">MAY 2001</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Music School - DCMI Diploma</strong>
+                <div class="caption mb-2">PRO MUSICA - Le Thor (France)</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">JUL 1982 -</div>
+                <div class="caption font-weight-bold green--text">AUG 1998</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Learned a lot of things</strong>
+                <div class="caption mb-2">SCHOOL OF LIFE</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card-text>
+    </v-flex>
+    <v-flex xs12 sm12 md4 lg6 xl6 class="mt-4 pt-2">
+      <h2 class="mb-4 pl-4">Experience</h2>
+      <v-card-text class="py-0">
+        <v-timeline align-top dense>
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">JAN 2022 -</div>
+                <div class="caption font-weight-bold green--text">PRESENT</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Freelance Full-stack Developer</strong>
+                <div class="caption mb-2">Remote</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">JAN 2022 -</div>
+                <div class="caption font-weight-bold green--text">PRESENT</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>CMS Development</strong>
+                <div class="caption mb-2">e-shop development</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+
+          <v-timeline-item color="green" small>
+            <v-layout pt-3>
+              <v-flex xs4>
+                <div class="caption font-weight-bold green--text">AUG 2001 -</div>
+                <div class="caption font-weight-bold green--text">NOV 2021</div>
+              </v-flex>
+              <v-flex class="pl-2">
+                <strong>Musician</strong>
+                <div class="caption mb-2">WOOBIES BROTHERS // JAYADEVA</div>
+              </v-flex>
+            </v-layout>
+          </v-timeline-item>
+        </v-timeline>
+      </v-card-text>
+    </v-flex>
+    <v-layout row justify-center align-center wrap class="mt-4 pt-2">
+      <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
+        <h2>
+          <span>FRONTEND</span>
+
+          <span class="green--text">Skills</span>
+        </h2>
+        <br>
+        <strong>Html / Css</strong>
+        <v-progress-linear v-model="buffer1" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>Sass</strong>
+        <v-progress-linear v-model="buffer2" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>VueJs</strong>
+        <v-progress-linear v-model="buffer3" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+      </v-flex>
+      <v-flex xs12 sm12 md4 lg5 xl5 class="mx-2 pt-4">
+        <h2>
+          <span>BACKEND</span>
+          <span class="green--text">Skills</span>
+        </h2>
+        <br>
+        <strong>NodeJs</strong>
+        <v-progress-linear v-model="buffer4" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>MySql</strong>
+        <v-progress-linear v-model="buffer5" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+        <strong>MongoDB</strong>
+        <v-progress-linear v-model="buffer6" :buffer-value="bufferValue" color="green" height="16"></v-progress-linear>
+      </v-flex>
+      <div class="mt-4 pt-2">
+        <v-btn
+          href="https://drive.google.com/file/d/1_6f63ItFhhV0dFxkMvgEkyAd5XR6FFGv/view?usp=sharing"
+          target="_blank"
+          color="green"
+          dark
+          large
+        >
+          Download Resume
+          <v-icon right dark>fas fa-file-download</v-icon>
+        </v-btn>
+      </div>
+    </v-layout>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Resume",
+    titleTemplate: "%s ← Katou's Place",
+    meta: [
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        name: "description",
+        content:
+          "Vue NUXT HTML CSS JavaScript Design Adobe XD Photoshop Coding Skills Resume Work Web Developer Front-End"
+      },
+      { charset: "utf-8" },
+      { property: "og:title", content: "Eldin' Space" },
+      { property: "og:site_name", content: "Eldin' Space" },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://eldin.space" },
+      {
+        property: "og:image",
+        content: "./img/Dcz2PGx.jpg"
+      },
+      {
+        property: "og:description",
+        content:
+          "Vue NUXT HTML CSS JavaScript Design Adobe XD Photoshop Coding Skills Resume Work Web Developer Front-End"
+      }
+    ]
+  },
+  data() {
+    return {
+      value: 0,
+      buffer1: 0,
+      buffer2: 0,
+      buffer3: 0,
+      buffer4: 0,
+      buffer5: 0,
+      buffer6: 0,
+      bufferValue: 100,
+      interval: 0
+    };
+  },
+  mounted() {
+    this.startBuffer();
+  },
+
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+
+  methods: {
+    startBuffer() {
+      this.interval = setInterval(() => {
+        this.buffer1 = 90;
+        this.buffer2 = 85;
+        this.buffer3 = 80;
+        this.buffer4 = 80;
+        this.buffer5 = 90;
+        this.buffer6 = 70;
+      }, 800);
+    }
+  }
+};
+</script>
+<style >
+</style>
+
